57dd533331adba29e919dbe98bdb50f822fb19c1
lionelgendebien <lionelgendebien@gmail.com>
2026-01-09 18:59:54 +0100

    updated processing pipeline

--- process_target_data/pipeline.py
+++ process_target_data/pipeline.py
@@ -104,20 +104,24 @@ def get_iaso_org_unit_tree() -> pd.DataFrame:
     """
     current_run.log_info("Retrieving org unit tree data from IASO...")
 
-    # iaso_connector_instance = IASOConnectionHandler(iaso_connector_slug)
-    # iaso_org_unit_tree_df = iaso_connector_instance.get_ou_tree_dataframe_from_the_form(
-    #     iaso_form_id
-    # )
+    iaso_connector_instance = IASOConnectionHandler(iaso_connector_slug)
+    iaso_org_unit_tree_df = iaso_connector_instance.get_ou_tree_dataframe_from_the_form(
+        iaso_form_id
+    )
 
     # save file to parquet for later use
     file_path = os.path.join(
-        workspace.files_path, "output", "iaso_org_unit_tree_raw.parquet"
+        workspace.files_path,
+        "niger_june_2024",
+        "process_target_data",
+        "output",
+        "iaso_org_unit_tree_raw.parquet",
     )
-    # Path(file_path).parent.mkdir(parents=True, exist_ok=True)
-    # iaso_org_unit_tree_df.to_parquet(
-    #     file_path,
-    #     index=False,
-    # )
+    Path(file_path).parent.mkdir(parents=True, exist_ok=True)
+    iaso_org_unit_tree_df.to_parquet(
+        file_path,
+        index=False,
+    )
     iaso_org_unit_tree_df = pd.read_parquet(file_path)
 
     return iaso_org_unit_tree_df
